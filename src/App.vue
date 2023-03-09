167e0795c040620e203056bb92df9542dba7cf48
dunzolo <rossidavide108@gmail.com>
2023-03-09 15:35:03 +0100

    milestone 4 done: - added AppProjects

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script>
-export default {
+import AppProject from './components/AppProject.vue';
 
+export default {
+  components: {
+    AppProject
+  }
 }
 </script>
 
 <template lang="">
   <div>
-    
+    <AppProject/>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+@use './style/generals.scss' as *;
+</style>
