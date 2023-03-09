2ff448d9ab21e8818a7c8008e8c8037f32f13f25
dunzolo <rossidavide108@gmail.com>
2023-03-09 16:55:43 +0100

    📖 fixed style card - added margin tag - added image object-fit cover

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -25,7 +25,7 @@ export default {
                     <div class="mb-2"><em><strong>Categoria: </strong>{{project.type.name}}</em></div>
                     <div class="mb-2">
                         <em><strong>Tecnologia: </strong>
-                            <span class="badge bg-primary" v-for="(technology, index) in project.technologies" :key="index">{{technology.name}}</span>
+                            <span class="badge bg-primary mx-1" v-for="(technology, index) in project.technologies" :key="index">{{technology.name}}</span>
                         </em>
                     </div>
                     <p class="card-text">{{this.substringContent(project.content)}}</p>
@@ -49,6 +49,7 @@ export default {
     img {
         height: 100%;
         width: 100%;
+        object-fit: cover;
     }
 }
 </style>
